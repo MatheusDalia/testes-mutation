35a24411a4ec3a04b3a4c35b021502318e826890
titi-lima <titisau@gmail.com>
2025-12-09 21:00:00 -0300

    adds js-utils formatter

--- packages/is/isBoolean/test.js
+++
@@ -1,9 +0,0 @@
-const isBoolean = require('./isBoolean');
-
-describe('is/isBoolean', () => {
-  it('checks if a value is boolean', () => {
-    expect(isBoolean(true)).toBe(true);
-
-    expect(isBoolean(100)).toBe(false);
-  });
-});
--- packages/is/isDate/test.js
+++
@@ -1,9 +0,0 @@
-const isDate = require('./isDate');
-
-describe('is/isDate', () => {
-  it('checks if a value is Date object', () => {
-    expect(isDate(new Date())).toBe(true);
-
-    expect(isDate(new Date().getTime())).toBe(false);
-  });
-});
